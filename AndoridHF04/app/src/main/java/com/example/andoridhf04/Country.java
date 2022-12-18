32dc99dc5e52b4d94cf80e603d84f34187893c06
Telexx12 <telegdi.david12@gmail.com>
2022-12-18 02:15:25 +0200

    HF

---
+++ AndoridHF04/app/src/main/java/com/example/andoridhf04/Country.java
@@ -0,0 +1,16 @@
+package com.example.andoridhf04;
+
+public class Country {
+
+    String nev, rovidNev;
+    Double elad,vesz;
+    int imageID;
+
+    public Country(String nev, String rovidNev, Double elad, Double vesz, int imageID) {
+        this.nev = nev;
+        this.rovidNev = rovidNev;
+        this.elad = elad;
+        this.vesz = vesz;
+        this.imageID = imageID;
+    }
+}
